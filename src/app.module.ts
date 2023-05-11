2f7bda892563a3c7c56f906f9437253aa1bd3331
dahee jang <wol04255@gmail.com>
2023-05-11 22:03:43 +0900

    ormconfig 삭제, env만 이용하여 db연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,33 @@ import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import ormconfig = require('../ormconfig');
 import { DataSource } from 'typeorm';
 import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { Movie } from './movies/entities/movie.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [MoviesModule, TypeOrmModule.forRoot(ormconfig), UsersModule],
+  imports: [
+    MoviesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User, Movie],
+      synchronize: true,
+      autoLoadEntities: true,
+      charset: 'utf8mb4',
+      logging: true,
+      keepConnectionAlive: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
